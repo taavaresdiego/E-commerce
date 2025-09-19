146403561416c8337259001d896915fd171f98d4
Lucas Caitano <lucacaitano2001@gmail.com>
2025-09-19 20:54:43 -0300

    correção das pastas e arquivos: views e usuario.

--- src/views/Produto.vue
+++ src/views/Produto.vue
@@ -3,25 +3,21 @@
     <div v-if="produto" class="produto">
       <ul class="fotos" v-if="produto.fotos">
         <li v-for="(foto, index) in produto.fotos" :key="index">
-          <img :src="foto.src" :alt="foto.titulo" />
+          <img :src="foto.src" :alt="foto.titulo">
         </li>
       </ul>
       <div class="info">
-        <h1>{{ produto.nome }}</h1>
-        <p class="preco">{{ produto.preco | numeroPreco }}</p>
-        <p class="descricao">{{ produto.descricao }}</p>
+        <h1>{{produto.nome}}</h1>
+        <p class="preco">{{produto.preco | numeroPreco}}</p>
+        <p class="descricao">{{produto.descricao}}</p>
         <transition mode="out-in" v-if="produto.vendido === 'false'">
-          <button class="btn" v-if="!finalizar" @click="finalizar = true">
-            Comprar
-          </button>
-          <FinalizarCompra v-else :produto="produto" />
+          <button class="btn" v-if="!finalizar" @click="finalizar = true">Comprar</button>
+          <FinalizarCompra v-else :produto="produto"/>
         </transition>
-        <button v-else class="btn btn-disabled" disabled>
-          Produto Vendido
-        </button>
+        <button v-else class="btn btn-disabled" disabled>Produto Vendido</button>
       </div>
     </div>
-    <PaginaCarregando v-else />
+    <PaginaCarregando v-else/>
   </section>
 </template>
 
@@ -33,25 +29,25 @@ export default {
   name: "Produtos",
   props: ["id"],
   components: {
-    FinalizarCompra,
+    FinalizarCompra
   },
   data() {
     return {
       produto: null,
-      finalizar: false,
+      finalizar: false
     };
   },
   methods: {
     getProduto() {
-      api.get(`/produto/${this.id}`).then((response) => {
+      api.get(`/produto/${this.id}`).then(response => {
         this.produto = response.data;
         document.title = this.produto.nome;
       });
-    },
+    }
   },
   created() {
     this.getProduto();
-  },
+  }
 };
 </script>
 
